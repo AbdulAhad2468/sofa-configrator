b5f3a58fbee4cda07d5821d4766237ae18317eba
Your Name <abdulahad2086907@gmail.com>
2026-08-08 15:26:52 +0500

    Add model-viewer AR page and route View at Home

--- script.js
+++ script.js
@@ -3,13 +3,5 @@
   }
         function startAR() {
             const currentModel = document.getElementById('modelSelect').value;
-            const modelUrl = `${window.location.origin}/${currentModel}`;
-            const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-            if (isIOS) {
-                window.location.href = modelUrl.replace('.glb', '.usdz');
-            } else {
-                const encodedUrl = encodeURIComponent(modelUrl);
-                window.location.href = `https://arvr.google.com/scene-viewer/1.0?file=${encodedUrl}&mode=ar_preferred`;
-            }
+            window.location.href = `ar.html?model=${encodeURIComponent(currentModel)}`;
         }
