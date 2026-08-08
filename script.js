57f02f7a8b57686c52ef749ff705fb197a675096
Your Name <abdulahad2086907@gmail.com>
2026-08-08 15:14:17 +0500

    Use Google Scene Viewer web URL for AR

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
   function handleViewAtHomeDesktop() {
-      const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-      if (isMobile) { startAR(); }
-      else { alert("The 'View at Home' AR feature is available on mobile devices."); }
+      startAR();
   }
         function startAR() {
             const currentModel = document.getElementById('modelSelectMobile').value;
@@ -12,7 +10,6 @@
                 window.location.href = modelUrl.replace('.glb', '.usdz');
             } else {
                 const encodedUrl = encodeURIComponent(modelUrl);
-                const fallbackUrl = encodeURIComponent(window.location.href);
-                window.location.href = `intent://arvr.google.com/scene-viewer/1.0?file=${encodedUrl}&mode=ar_preferred#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=${fallbackUrl};end;`;
+                window.location.href = `https://arvr.google.com/scene-viewer/1.0?file=${encodedUrl}&mode=ar_preferred`;
             }
         }
