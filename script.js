0a19357597b9b7ba9e19687387f4a283109737fe
Your Name <abdulahad2086907@gmail.com>
2026-08-08 14:55:50 +0500

    Deploy sofa configrator site

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
   function handleViewAtHomeDesktop() { alert("The 'View at Home' AR feature is available on mobile devices."); }
         function startAR() {
             const currentModel = document.getElementById('modelSelectMobile').value;
+            const modelUrl = `${window.location.origin}/${currentModel}`;
             const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-            if (isIOS) { window.location.href = `./models/${currentModel.replace('.glb', '.usdz')}`; } 
-            else { window.location.href = `intent://arvr.google.com/scene-viewer/1.0?file=${window.location.origin}/models/${currentModel}&mode=ar_only#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;end;`; }
+
+            if (isIOS) {
+                window.location.href = modelUrl.replace('.glb', '.usdz');
+            } else {
+                const encodedUrl = encodeURIComponent(modelUrl);
+                const fallbackUrl = encodeURIComponent(window.location.href);
+                window.location.href = `intent://arvr.google.com/scene-viewer/1.0?file=${encodedUrl}&mode=ar_preferred#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=${fallbackUrl};end;`;
+            }
         }
